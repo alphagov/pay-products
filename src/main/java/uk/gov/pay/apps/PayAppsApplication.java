6cb4e7f39b9ec61f527cce71624391270979c548
Kasun Kumara <kasun.kumara@digital.cabinet-office.gov.uk>
2017-09-06 15:31:31 +0100

    PP-2586 adding metrics support
    with @georgeracu

--- src/main/java/uk/gov/pay/apps/PayAppsApplication.java
+++ src/main/java/uk/gov/pay/apps/PayAppsApplication.java
@@ -1,5 +1,8 @@
 package uk.gov.pay.apps;
 
+import com.codahale.metrics.graphite.GraphiteReporter;
+import com.codahale.metrics.graphite.GraphiteSender;
+import com.codahale.metrics.graphite.GraphiteUDP;
 import io.dropwizard.Application;
 import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
 import io.dropwizard.configuration.SubstitutingSourceProvider;
@@ -11,9 +14,12 @@ import uk.gov.pay.apps.util.TrustingSSLSocketFactory;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSocketFactory;
+import java.util.concurrent.TimeUnit;
 
 public class PayAppsApplication extends Application<PayAppsConfiguration> {
     private static final boolean NON_STRICT_VARIABLE_SUBSTITUTOR = false;
+    private static final String SERVICE_METRICS_NODE = "pay-apps";
+    private static final int GRAPHITE_SENDING_PERIOD_SECONDS = 10;
 
     @Override
     public String getName() {
@@ -33,6 +39,7 @@ public class PayAppsApplication extends Application<PayAppsConfiguration> {
     public void run(final PayAppsConfiguration configuration,
                     final Environment environment) {
         environment.healthChecks().register("ping", new Ping());
+        initialiseMetrics(configuration, environment);
 
         setGlobalProxies();
     }
@@ -40,6 +47,15 @@ public class PayAppsApplication extends Application<PayAppsConfiguration> {
     private void setGlobalProxies() {
         SSLSocketFactory socketFactory = new TrustingSSLSocketFactory();
         HttpsURLConnection.setDefaultSSLSocketFactory(socketFactory);
+    }
+
+    private void initialiseMetrics(PayAppsConfiguration configuration, Environment environment) {
+        GraphiteSender graphiteUDP = new GraphiteUDP(configuration.getGraphiteHost(), Integer.valueOf(configuration.getGraphitePort()));
+        GraphiteReporter.forRegistry(environment.metrics())
+                .prefixedWith(SERVICE_METRICS_NODE)
+                .build(graphiteUDP)
+                .start(GRAPHITE_SENDING_PERIOD_SECONDS, TimeUnit.SECONDS);
+
     }
 
     public static void main(final String[] args) throws Exception {
--- src/main/java/uk/gov/pay/apps/config/PayAppsConfiguration.java
+++ src/main/java/uk/gov/pay/apps/config/PayAppsConfiguration.java
@@ -2,10 +2,25 @@ package uk.gov.pay.apps.config;
 
 import io.dropwizard.Configuration;
 
+import javax.validation.constraints.NotNull;
+
 public class PayAppsConfiguration extends Configuration {
+
+    @NotNull
+    private String graphiteHost;
+    @NotNull
+    private String graphitePort;
 
     public String getVcapServices() {
         String vcapServices = System.getenv("VCAP_SERVICES");
         return vcapServices;
+    }
+
+    public String getGraphiteHost() {
+        return graphiteHost;
+    }
+
+    public String getGraphitePort() {
+        return graphitePort;
     }
 }

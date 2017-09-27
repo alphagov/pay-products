f1230ccf49973277904fed58149cc82fe6faae72
george racu <george.racu@digital.cabinet-office.gov.uk>
2017-09-27 17:26:18 +0100

    PP-2668 Added ProductStatus as Enum
    with @kakumara

---
+++ src/main/java/uk/gov/pay/products/util/ProductStatus.java
@@ -0,0 +1,8 @@
+package uk.gov.pay.products.util;
+
+/**
+ * Status field to be used for {@link uk.gov.pay.products.persistence.entity.ProductEntity}
+ */
+public enum ProductStatus {
+    ACTIVE
+}

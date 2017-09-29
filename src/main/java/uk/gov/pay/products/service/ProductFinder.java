d21d16deb1eb3b52ddbe99a883190eaa32232cb7
george racu <george.racu@digital.cabinet-office.gov.uk>
2017-09-29 21:53:09 +0100

    PP-2669 Added ProductFinder
    - Added ProductFinder and hooked into DI

---
+++ src/main/java/uk/gov/pay/products/service/ProductFinder.java
@@ -0,0 +1,23 @@
+package uk.gov.pay.products.service;
+
+import com.google.inject.Inject;
+import uk.gov.pay.products.model.Product;
+import uk.gov.pay.products.persistence.dao.ProductDao;
+
+import java.util.Optional;
+
+public class ProductFinder {
+    private final ProductDao productDao;
+    private final LinksDecorator linksDecorator;
+
+    @Inject
+    public ProductFinder(ProductDao productDao, LinksDecorator linksDecorator) {
+        this.productDao = productDao;
+        this.linksDecorator = linksDecorator;
+    }
+
+    public Optional<Product> findByExternalId(String externalId) {
+        return productDao.findByExternalId(externalId)
+                .map(productEntity -> linksDecorator.decorate(productEntity.toProduct()));
+    }
+}

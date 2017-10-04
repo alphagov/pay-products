2c98db41002ba21070d3a28535f9be0bdd8cff77
george racu <george.racu@digital.cabinet-office.gov.uk>
2017-10-04 15:25:07 +0100

    PP-2671 Removed `merge`
    - Removed object `merge`;
    - Changed code the follow the pattern `Optional<T>.map.orElseGet`

--- src/main/java/uk/gov/pay/products/service/ProductFinder.java
+++ src/main/java/uk/gov/pay/products/service/ProductFinder.java
@@ -4,7 +4,6 @@ import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
 import uk.gov.pay.products.model.Product;
 import uk.gov.pay.products.persistence.dao.ProductDao;
-import uk.gov.pay.products.persistence.entity.ProductEntity;
 import uk.gov.pay.products.util.ProductStatus;
 
 import java.util.Optional;
@@ -27,14 +26,11 @@ public class ProductFinder {
 
     @Transactional
     public Optional<Product> disableProduct(String externalId) {
-        Optional<ProductEntity> productEntity = productDao.findByExternalId(externalId);
-        if(productEntity.isPresent()){
-            ProductEntity product = productEntity.get();
-            product.setStatus(ProductStatus.INACTIVE);
-            productDao.merge(product);
-            return Optional.of(product.toProduct());
-        }
-
-        return Optional.empty();
+        return productDao.findByExternalId(externalId)
+                .map(productEntity -> {
+                    productEntity.setStatus(ProductStatus.INACTIVE);
+                    return Optional.of(productEntity.toProduct());
+                })
+                .orElseGet(Optional::empty);
     }
 }

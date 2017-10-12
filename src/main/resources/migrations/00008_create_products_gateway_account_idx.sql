9fdb65a4f6cc70ddc4aa5a556fa37d5fc991b251
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2017-10-12 10:31:45 +0100

    PP-2672 Requested fix of potential bug

--- src/main/resources/migrations/00008_create_products_gateway_account_idx.sql
+++ src/main/resources/migrations/00008_create_products_gateway_account_idx.sql
@@ -3,4 +3,4 @@
 --changeset uk.gov.pay:add_index_to_gateway_account_id_on_products
 CREATE INDEX gateway_account_idx ON products(gateway_account_id);
 
---rollback alter table products drop column gateway_account_id
+--rollback drop index gateway_account_idx on products

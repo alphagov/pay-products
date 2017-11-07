663b20d38072a7ae19f9dd4ebb03a3b62416d0f5
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2017-11-07 15:40:14 +0000

    PP-2672 Updated RequestValidations
    - Add handling of return url. When return url is not present (prototype case) then we add a pre-defined return url
    
    with @ckalista

--- src/test/java/uk/gov/pay/products/service/PaymentCreatorTest.java
+++ src/test/java/uk/gov/pay/products/service/PaymentCreatorTest.java
@@ -3,14 +3,16 @@ package uk.gov.pay.products.service;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import uk.gov.pay.products.client.publicapi.PaymentRequest;
 import uk.gov.pay.products.client.publicapi.PaymentResponse;
 import uk.gov.pay.products.client.publicapi.PublicApiRestClient;
 import uk.gov.pay.products.client.publicapi.model.Link;
 import uk.gov.pay.products.client.publicapi.model.Links;
+import uk.gov.pay.products.config.ProductsConfiguration;
 import uk.gov.pay.products.exception.PaymentCreationException;
 import uk.gov.pay.products.exception.PaymentCreatorNotFoundException;
 import uk.gov.pay.products.exception.PublicApiResponseErrorException;
@@ -23,6 +25,7 @@ import uk.gov.pay.products.persistence.entity.PaymentEntity;
 import uk.gov.pay.products.persistence.entity.ProductEntity;
 import uk.gov.pay.products.service.transaction.TransactionFlow;
 import uk.gov.pay.products.util.PaymentStatus;
+import uk.gov.pay.products.util.RandomIdGenerator;
 
 import java.util.Optional;
 
@@ -36,7 +39,8 @@ import static org.mockito.hamcrest.MockitoHamcrest.argThat;
 import static uk.gov.pay.products.util.PaymentStatus.ERROR;
 import static uk.gov.pay.products.util.PaymentStatus.SUCCESS;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(RandomIdGenerator.class)
 public class PaymentCreatorTest {
     static private String PRODUCT_URL = "https://products.url";
     static private String PRODUCT_UI_URL = "https://products-ui.url";
@@ -50,16 +54,20 @@ public class PaymentCreatorTest {
     @Mock
     private PublicApiRestClient publicApiRestClient;
 
+    @Mock
+    private ProductsConfiguration productsConfiguration;
+
     private PaymentCreator paymentCreator;
 
     @Before
     public void setup() throws Exception {
         LinksDecorator linksDecorator = new LinksDecorator(PRODUCT_URL, PRODUCT_UI_URL);
-        paymentCreator = new PaymentCreator(TransactionFlow::new, productDao, paymentDao, publicApiRestClient, linksDecorator);
+        paymentCreator = new PaymentCreator(TransactionFlow::new, productDao, paymentDao, publicApiRestClient, linksDecorator, productsConfiguration);
+        PowerMockito.mockStatic(RandomIdGenerator.class);
     }
 
     @Test
-    public void shouldCreateASuccessfulPayment() throws Exception {
+    public void shouldCreateASuccessfulPayment_whenReturnUrlIsPresent() throws Exception {
         int productId = 1;
         String productExternalId = "product-external-id";
         long productPrice = 100L;
@@ -68,6 +76,7 @@ public class PaymentCreatorTest {
         String productApiToken = "api-token";
 
         String paymentId = "payment-id";
+        String paymentExternalId = "random-external-id";
         Long paymentAmount = 50L;
         String paymentNextUrl = "http://next.url";
 
@@ -86,10 +95,76 @@ public class PaymentCreatorTest {
         PaymentResponse paymentResponse = createPaymentResponse(
                 paymentId,
                 paymentAmount,
-                paymentNextUrl);
+                paymentNextUrl,
+                productReturnUrl);
 
 
         when(productDao.findByExternalId(productExternalId)).thenReturn(Optional.of(productEntity));
+        when(RandomIdGenerator.randomUuid()).thenReturn(paymentExternalId);
+        when(publicApiRestClient.createPayment(argThat(is(productApiToken)), argThat(PaymentRequestMatcher.isSame(expectedPaymentRequest)))).thenReturn(paymentResponse);
+
+        Payment payment = paymentCreator.doCreate(productExternalId);
+
+        assertNotNull(payment);
+        assertNotNull(payment.getExternalId());
+        assertThat(payment.getGovukPaymentId(), is(paymentResponse.getPaymentId()));
+        assertThat(payment.getNextUrl(), is(paymentResponse.getLinks().getNextUrl().getHref()));
+        assertThat(payment.getAmount(), is(paymentResponse.getAmount()));
+        assertNotNull(payment.getLinks());
+        assertThat(payment.getLinks().size(), is(2));
+        assertThat(payment.getLinks().get(0).getMethod(), is("GET"));
+        assertThat(payment.getLinks().get(0).getHref(), is(PRODUCT_URL + "/v1/api/payments/" + payment.getExternalId()));
+        assertThat(payment.getLinks().get(1).getMethod(), is("GET"));
+        assertThat(payment.getLinks().get(1).getHref(), is(paymentResponse.getLinks().getNextUrl().getHref()));
+        assertThat(payment.getProductId(), is(productEntity.getId()));
+        assertThat(payment.getProductExternalId(), is(productEntity.getExternalId()));
+        assertThat(payment.getStatus(), is(SUCCESS));
+
+        PaymentEntity expectedPaymentEntity = createPaymentEntity(
+                paymentId,
+                paymentNextUrl,
+                productEntity,
+                SUCCESS,
+                paymentAmount);
+        verify(paymentDao).merge(argThat(PaymentEntityMatcher.isSame(expectedPaymentEntity)));
+    }
+
+    @Test
+    public void shouldCreateASuccessfulPayment_whenReturnUrlIsNotPresent() throws Exception {
+        int productId = 1;
+        String productExternalId = "product-external-id";
+        long productPrice = 100L;
+        String productName = "name";
+        String productReturnUrl = "https://return.url";
+        String productApiToken = "api-token";
+
+        String paymentId = "payment-id";
+        String paymentExternalId = "random-external-id";
+        Long paymentAmount = 50L;
+        String paymentNextUrl = "http://next.url";
+
+        ProductEntity productEntity = createProductEntity(
+                productId,
+                productPrice,
+                productExternalId,
+                productName,
+                "",
+                productApiToken);
+        PaymentRequest expectedPaymentRequest = createPaymentRequest(
+                productPrice,
+                productExternalId,
+                productName,
+                productReturnUrl + "/" + paymentExternalId);
+        PaymentResponse paymentResponse = createPaymentResponse(
+                paymentId,
+                paymentAmount,
+                paymentNextUrl,
+                productReturnUrl);
+
+
+        when(productDao.findByExternalId(productExternalId)).thenReturn(Optional.of(productEntity));
+        when(RandomIdGenerator.randomUuid()).thenReturn(paymentExternalId);
+        when(productsConfiguration.getProductsUiConfirmUrl()).thenReturn(productReturnUrl);
         when(publicApiRestClient.createPayment(argThat(is(productApiToken)), argThat(PaymentRequestMatcher.isSame(expectedPaymentRequest)))).thenReturn(paymentResponse);
 
         Payment payment = paymentCreator.doCreate(productExternalId);
@@ -142,6 +217,7 @@ public class PaymentCreatorTest {
                 productReturnUrl);
 
         when(productDao.findByExternalId(productExternalId)).thenReturn(Optional.of(productEntity));
+        when(RandomIdGenerator.randomUuid()).thenReturn(productExternalId);
         when(publicApiRestClient.createPayment(argThat(is(productApiToken)), argThat(PaymentRequestMatcher.isSame(expectedPaymentRequest))))
                 .thenThrow(PublicApiResponseErrorException.class);
 
@@ -163,6 +239,7 @@ public class PaymentCreatorTest {
     @Test
     public void shouldThrowPaymentCreatorNotFoundException_whenProductIsNotFound() throws Exception {
         String productExternalId = "product-external-id";
+        when(RandomIdGenerator.randomUuid()).thenReturn(productExternalId);
 
         when(productDao.findByExternalId(productExternalId)).thenReturn(Optional.empty());
 
@@ -188,13 +265,14 @@ public class PaymentCreatorTest {
         return productEntity;
     }
 
-    private PaymentResponse createPaymentResponse(String id, Long amount, String link) {
+    private PaymentResponse createPaymentResponse(String id, Long amount, String link, String returnUrl) {
         Links links = new Links();
         links.setNextUrl(new Link(link, "GET", "multipart/form-data"));
         PaymentResponse paymentResponse = new PaymentResponse();
         paymentResponse.setPaymentId(id);
         paymentResponse.setAmount(amount);
         paymentResponse.setLinks(links);
+        paymentResponse.setReturnUrl(returnUrl);
 
         return paymentResponse;
     }

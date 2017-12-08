cb22b36fac014b1a823994b129cc2b3e69f6c37d
Christophe Kalista <christophe.kalista@gmail.com>
2017-12-08 09:46:50 +0000

    PP-3032 Updated database migration to add the product `type` field
    Added the product `type` field of type `VARCHAR(255)` which backfills with default value of `DEMO`.

---
+++ src/main/resources/migrations/00011_add_column_type_to_products.sql
@@ -0,0 +1,6 @@
+--liquibase formatted sql
+
+--changeset uk.gov.pay:add_column_type_on_products
+ALTER TABLE products ADD COLUMN type VARCHAR(255) NOT NULL DEFAULT 'DEMO';
+
+--rollback alter table products drop column type;

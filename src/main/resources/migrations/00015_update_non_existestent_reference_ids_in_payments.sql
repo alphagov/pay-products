273bdbe2287c66a5fd5407916ef8d176721dfaef
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2017-12-11 16:24:45 +0000

    PP-3031 Migrate data to payments
    - migrate gateway_account_id from products to corresponding payments
    - back fill reference_number on payments

---
+++ src/main/resources/migrations/00015_update_non_existestent_reference_ids_in_payments.sql
@@ -0,0 +1,5 @@
+--liquibase formatted sql
+
+--changeset uk.gov.pay:update_non_existestent_reference_ids_in_payments
+
+UPDATE payments SET reference_number = upper(substring(replace(uuid_generate_v4()::VARCHAR,'-',''), 1, 10)) WHERE reference_number IS NULL;

4ab481f0d4d0cc8b20e9c5baf5eacbffc1b428f8
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-01-15 18:14:57 +0000

    BAU: Remove unused newId() from RandomIdGenerator

--- src/main/java/uk/gov/pay/products/util/RandomIdGenerator.java
+++ src/main/java/uk/gov/pay/products/util/RandomIdGenerator.java
@@ -1,7 +1,5 @@
 package uk.gov.pay.products.util;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
 import java.util.Random;
 import java.util.UUID;
 
@@ -9,7 +7,6 @@ import static java.util.stream.IntStream.range;
 
 public class RandomIdGenerator {
 
-    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
     private static final Random RANDOM = new Random();
     /**
      * source
@@ -17,21 +14,6 @@ public class RandomIdGenerator {
     private static final String RANDOM_SOURCE_SET_1 = "ABCSDEFGHJKLMNPQRSTUVWXYZ";
     private static final String RANDOM_SOURCE_SET_2 = "23456789";
     private static final String RANDOM_SOURCE_UNION = RANDOM_SOURCE_SET_1.concat(RANDOM_SOURCE_SET_2);
-
-    /**
-     * This method will generate a URL safe random string.
-     * This works by choosing 130 bits from a cryptographically secure random bit generator,
-     * and encoding them in base-32.
-     * <p> 128 bits is considered to be cryptographically strong,
-     * but each digit in a base 32 number can encode 5 bits, so 128 is rounded up to the next multiple of 5.
-     * This encoding is compact and efficient, with 5 random bits per character. Compare this to a random UUID,
-     * which only has 3.4 bits per character in standard layout, and only 122 random bits in total </p>
-     *
-     * @return a random number in base32 (in string format)
-     */
-    public static String newId() {
-        return new BigInteger(130, SECURE_RANDOM).toString(32);
-    }
 
     public static Integer randomInt() {
         return RANDOM.nextInt(Integer.MAX_VALUE);

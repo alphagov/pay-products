483a23d0396ae840833e51ac225443a2e34f5c1f
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-04-24 18:31:01 +0100

    Remove unused ConflictingPaymentRequestException
    This has been unused since ec8fbe2a3c35335e3b1b7cf9879921b9421c7110

--- src/main/java/uk/gov/pay/products/exception/ConflictingPaymentRequestException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.pay.products.exception;
-
-public class ConflictingPaymentRequestException extends RuntimeException {
-    
-    public ConflictingPaymentRequestException(String message) {
-        super(message);
-    }
-}

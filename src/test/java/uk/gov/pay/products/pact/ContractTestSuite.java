46bb250ddcf68d6dd078ea50b85b9f350b396455
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2019-10-02 16:16:38 +0100

    PP-5670: Specify the consumer when running contract test
    Currently when a consumer build is kicked off, the provider contract test will
    run in its entirety. For example, in a selfservice build, the products provider
    contract tests will run as well. However, what is run is:
    
    products-ui->products
    selfservice->products
    
    We really only want to run selfservice->products in this case.
    
    This commit will allow a dynamic creation of the test suite to run based on a
    CONSUMER system property. This will mean shorter run times when a provider
    contract test is run as part of a specific consumer build.

--- src/test/java/uk/gov/pay/products/pact/ContractTestSuite.java
+++ src/test/java/uk/gov/pay/products/pact/ContractTestSuite.java
@@ -1,11 +1,20 @@
 package uk.gov.pay.products.pact;
 
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.runners.AllTests;
+import uk.gov.pay.commons.testing.pact.provider.CreateTestSuite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-        ProviderContractTest.class
-})
+import java.util.Map;
+
+@RunWith(AllTests.class)
 public class ContractTestSuite {
+
+    public static TestSuite suite() {
+        Map<String, JUnit4TestAdapter> map = Map.of(
+                "products-ui", new JUnit4TestAdapter(ProductsUIContractTest.class),
+                "selfservice", new JUnit4TestAdapter(SelfServiceContractTest.class));
+        return CreateTestSuite.create(map);
+    }
 }

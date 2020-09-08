eb2d241588f97edc2be708d550752e92af0c531c
Rory Malcolm <rory.malcolm@digital.cabinet-office.gov.uk>
2020-09-08 09:03:26 +0100

    PP-7020 Fix Products Integration Tests
    - Remove an unused and breaking test for products as well as the unused
    methods in the Dao classes.
    
    - Add .secrets.baseline

--- src/main/java/uk/gov/pay/products/persistence/dao/ProductMetadataDao.java
+++ src/main/java/uk/gov/pay/products/persistence/dao/ProductMetadataDao.java
@@ -15,16 +15,6 @@ public class ProductMetadataDao extends JpaDao<ProductMetadataEntity> {
         super(entityManager);
     }
 
-    public List<ProductMetadataEntity> findByProductsId(Integer productId) {
-        String query = "SELECT metadata FROM ProductMetadataEntity metadata " +
-                "WHERE metadata.id = :productId";
-
-        return entityManager.get()
-                .createQuery(query, ProductMetadataEntity.class)
-                .setParameter("productId", productId)
-                .getResultList();
-    }
-
     public List<ProductMetadataEntity> findByProductsExternalId(String productExternalId) {
         String query = "SELECT metadata FROM ProductMetadataEntity metadata " +
                 "WHERE metadata.productEntity.externalId = :productExternalId ";

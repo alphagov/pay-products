d3062c5f549570f34439594e03f80e45a4fa1014
Sandor Arpa <sandor.arpa@digital.cabinet-office.gov.uk>
2024-02-16 15:43:10 +0000

    PP-11725 upgrade Dropwizard to version 3
    - Upgrade Dropwizard to v3 latest.
    
    - Use dropwizard-dependencies BOM, which removes the need to pull in some dependencies ourselves

--- src/main/java/uk/gov/pay/products/config/ProductsConfiguration.java
+++ src/main/java/uk/gov/pay/products/config/ProductsConfiguration.java
@@ -1,7 +1,7 @@
 package uk.gov.pay.products.config;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import io.dropwizard.Configuration;
+import io.dropwizard.core.Configuration;
 import io.dropwizard.db.DataSourceFactory;
 
 import javax.validation.Valid;

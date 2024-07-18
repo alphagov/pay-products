a14ae4a7ffaae564ddbdb5dd666c4cecabcad0ac
James Peacock <james.peacock@digital.cabinet-office.gov.uk>
2024-07-18 15:48:48 +0100

    PP-12712 Log misconfigured account error as WARN not ERROR
    Context: Payment links created from not-yet-configured accounts are causing Sentry noise so a Zendesk alert is preferable
    
    - Handle a misconfigured exception by logging at WARN level rather than as an ERROR
    - A splunk alert will be set up to look for these logs and alert Zendesk

--- src/main/java/uk/gov/pay/products/util/PublicAPIErrorCodes.java
+++ src/main/java/uk/gov/pay/products/util/PublicAPIErrorCodes.java
@@ -3,4 +3,5 @@ package uk.gov.pay.products.util;
 public class PublicAPIErrorCodes {
 
      public static final String CARD_NUMBER_IN_PAYMENT_LINK_REFERENCE_ERROR_CODE = "P0105";
+     public static final String ACCOUNT_NOT_LINKED_WITH_PSP_ERROR_CODE = "P0940";
 }
